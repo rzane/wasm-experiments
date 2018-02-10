2274b213292cf80be83b7a4902246aca1529e924
Ray Zane <ray@promptworks.com>
2018-02-10 16:26:23 -0500

    It works!

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,40 @@ extern crate stdweb;
 
 use stdweb::web::{
     INode,
-    document
+    document,
 };
+
+use stdweb::web::event::{
+    ClickEvent
+};
+
+use stdweb::web::IEventTarget;
+use stdweb::web::html_element::InputElement;
+use stdweb::unstable::TryInto;
 
 fn main() {
     stdweb::initialize();
 
-    let header = document().query_selector("h1").unwrap();
-    header.set_text_content("Hello from Rust!");
+    let button = document().query_selector("#submit").unwrap();
+
+    button.add_event_listener(move |_: ClickEvent| {
+        let input: InputElement = document().query_selector("#new-todo").unwrap().try_into().unwrap();
+        let value = input.value().into_string().unwrap();
+        append_todo(&value);
+    });
+
+    append_todo("Do the laundry!");
+    append_todo("Run the dishwasher");
+
+    stdweb::event_loop();
+}
+
+fn append_todo(text: &str) {
+    let doc = document();
+    let list = doc.query_selector("#todos").unwrap();
+
+    let item = doc.create_element("li");
+    item.set_text_content(text);
+
+    list.append_child(&item);
 }
